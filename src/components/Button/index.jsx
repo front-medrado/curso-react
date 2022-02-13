035c7c0a96fef10a04e3e212937729299d9ad7e0
front-medrado <victor.medradom@gmail.com>
2022-02-13 19:13:34 -0300

    Migrando Home e componentes para React Hooks

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,18 +1,11 @@
 import './styles.css'
-import { Component } from "react";
 
-export class Button extends Component {
-    render () {
-        const {text, onClick, disabled} = this.props;
-
-        return (
-            <button 
-            className='btn' 
-            onClick={onClick}
-            disabled={disabled}
-            >
-                {text}
-            </button>
-        )
-    }
-}
+export const Button = ({ text, onClick, disabled }) => (
+    <button
+        className='btn'
+        onClick={onClick}
+        disabled={disabled}
+    >
+        {text}
+    </button>
+);
